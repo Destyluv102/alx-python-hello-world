2d33293176d5d3597a238bbbe0a719c430582881
dexter <106240039+Destyluv102@users.noreply.github.com>
2022-11-28 19:50:05 +0000

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines an objecct attributes lookup function"""
-
-
-def lookup(obj):
-    """A function that returns the list of available attributes"""
-    return (dir(obj))
